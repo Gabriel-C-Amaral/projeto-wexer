dafb0950291fe622b002a70cd4278afb92383ef7
Gabriel-C-Amaral <115030972+Gabriel-C-Amaral@users.noreply.github.com>
2023-03-23 17:30:20 -0300

    Initial commit

---
+++ src/components/app/index.tsx
@@ -0,0 +1,12 @@
+import { Normalize } from 'styled-normalize'
+
+function App() {
+  return (
+    <div className="App">
+      <Normalize />
+      <h1>oi</h1>
+    </div>
+  )
+}
+
+export default App
